7d3e2b14feabe3f15f1afbb388e28501c28a3a54
Khurram Bhatti <khurram.waleed.bhatti@gmail.com>
2024-03-28 19:47:21 -0400

    added doc

--- src/test/java/com/webstaurantstore/scenarios/Cart.java
+++ src/test/java/com/webstaurantstore/scenarios/Cart.java
@@ -14,8 +14,30 @@ import com.webstaurantstore.app.ui.pages.SearchResultsPage;
 import com.webstaurantstore.core.Suite;
 import com.webstaurantstore.ui.Page;
 
+
+/**
+ * To Execute the test:
+ * 		a) either install Eclipse TestNG plugin or use IntelliJ
+ * 		b) or run from command line
+ * 			mvn clean install
+ * 			available options
+ * 				1. groups = Will work if test is tagged with group
+ * 				2. env = <DEV|QA|PROD> (by default uses PROD)
+ * 				3. browser = <CHROME|IE|FIREFOX> (be default uses CHROME)
+ * 
+ * Report is generated in ./target/Report.html
+ * Log is generated in ./target/Logs.html
+ * 
+ * @author kbhatti
+ *
+ */
 public class Cart extends Suite {
 
+	/**
+	 * NOTE: Added Page.logScreenshot() just for the purpose of displaying each page and is not needed
+	 * 
+	 * @throws IOException
+	 */
 	@Test
 	private void search() throws IOException {
 		SoftAssert softAssert = new SoftAssert();
